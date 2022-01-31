6b334b2bc82806df67cc21347b004bba6a04290d
younesbouchbouk <younesbouchbouk.py@gmail.com>
2022-01-31 21:25:25 +0100

    Final V

--- src/main/java/com/example/demo/relationControllers.java
+++ src/main/java/com/example/demo/relationControllers.java
@@ -4,6 +4,7 @@ import com.example.demo.Listeners.ProjectListener;
 import com.example.demo.models.Project;
 import com.example.demo.models.UserSession;
 import com.example.demo.models.db;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -14,10 +15,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
@@ -80,7 +78,7 @@ public  class relationControllers implements Initializable {
     private HBox addproject;
 
     @FXML
-    private HBox dashboard;
+    private VBox dashboard;
 
     @FXML
     private ScrollPane listeproject;
@@ -127,6 +125,146 @@ public  class relationControllers implements Initializable {
 
     @FXML
     private TextField AddTitleprj;
+    //------------------------------------Dashbord
+
+    @FXML
+    private Label label1;
+    private volatile boolean stop = false;
+
+    @FXML
+    private Label label2 ;
+
+
+    //////////////////////
+    @FXML
+    private Pane settings;
+
+    @FXML
+    private Text l3;
+    @FXML
+    private Text l2;
+    @FXML
+    private Text l1;
+
+    @FXML
+    private Text nbtask_label1;
+
+    @FXML
+    private Text nb_done_taskes;
+
+    @FXML
+    private Text nb_new_taskes;
+
+    @FXML
+    private Text nb_todo_taskes;
+
+
+
+
+    ////////////////////////--Dasboard---------//////////////////////////////////////////////////////////////////////////////////////////////
+    // time in dashbord
+    private void Timenow() {
+        Thread thread = new Thread(() -> {
+            SimpleDateFormat sdf = new SimpleDateFormat("hh:mm");
+            while (!stop) {
+                try {
+                    Thread.sleep(1000);
+
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                final String timenow = sdf.format(new Date());
+                Platform.runLater(() -> {
+                    label1.setText(timenow);
+                });
+            }
+        });
+        thread.start();
+    }
+
+    //date in dashbord
+    private void datenow() {
+        SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy");
+        String datenow = sdf.format(new Date());
+        label2.setText(datenow);
+
+    }
+///////////////////////////////////End here/////////////////////////////////////////////////////////
+
+    /////:/:/ EDITE PROFILS //////////////////////////////////////
+
+    @FXML
+    private TextField Fullname;
+    @FXML
+    private TextField temail;
+    @FXML
+    private PasswordField paswd;
+
+    @FXML
+    private PasswordField cpaswd;
+    @FXML
+    private Button btn2;
+    @FXML
+    private Label welcomeText;
+
+
+
+
+    public void onHelloButtonClick() {
+        ResultSet resultSet = null;
+        PreparedStatement preparedStatement = null;
+        Connection con = null;
+        //con =null;
+        int iduser= UserSession.instance.getID(); //int iduser=String.valueOf(UserSession.instance.getID());
+        String Full = Fullname.getText();
+        String Emaile = temail.getText();
+        String password = paswd.getText();
+        String newpassworde = cpaswd.getText();
+
+
+        if(Full=="" || Emaile=="" || password=="" || newpassworde==""  ){
+            System.out.println("error");
+
+        }else{
+
+            String Query1 ="select Password from javafx2.user where idUtilisateur ="+UserSession.instance.getID();
+            try {
+                preparedStatement = con.prepareStatement(Query1);
+                preparedStatement.setInt(1, iduser);
+                resultSet = preparedStatement.executeQuery();
+                if (!resultSet.equals(password)) {
+
+                    welcomeText.setText("oLD PASSWORD IS NOT TRUE");
+
+                } else {
+
+                    String Query = "UPDATE javafx2.user SET Full_Name=?,Email=?,Password=?,image=?,Gender=?,type=? WHERE idUtilisateur="+UserSession.instance.getID();
+                    System.out.println(password);
+                    System.out.println(Emaile);
+
+
+                    try {
+                        con= db.getConnection();
+                        preparedStatement = con.prepareStatement(Query, Statement.RETURN_GENERATED_KEYS);
+                        preparedStatement.setString(1, Full);
+                        preparedStatement.setString(2, Emaile);
+                        preparedStatement.setString(3, newpassworde);
+                        int Id;
+                        Id = preparedStatement.executeUpdate();
+                    } catch (SQLException e) {
+                        // TODO Auto-generated catch block
+                        welcomeText.setText("EREUR");
+                    }
+                }
+            }catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    ////// EDITE PROFILS ENDHERE ///////////////////////////////
 
     //==========================================================Gridpane
     @FXML
@@ -251,20 +389,46 @@ public  class relationControllers implements Initializable {
     private TextField Search_idprj;
 
     @FXML
+    private TextField s2searchFields;
+
+    @FXML
     private Label Search_title_label;
 
     @FXML
     private Button Serch_btn;
 
+    @FXML
+    private Button topbarbtn;
+
     int seachedteamid = -1;
+
+
 
     //end search declarion
 
-    public void HandleSearchProject() throws SQLException {
+    public void HandleSearchProject(ActionEvent event) throws SQLException {
 
-
+        String theprojectid ="";
         //int groupid = Integer.getInteger(Search_idprj.getText());
-        String theprojectid = Search_idprj.getText();
+        Object source = event.getSource();
+
+        if(Serch_btn.equals(source)){
+            theprojectid = Search_idprj.getText();
+            Search_idprj.clear();
+        }else if(topbarbtn.equals(source)){
+            theprojectid = s2searchFields.getText();
+            s2searchFields.clear();
+            pathfiletxt.setText("/dashboard/project/Jointeam");
+            settings.setVisible(false);
+            addproject.setVisible(false);
+            dashboard.setVisible(false);
+            listeproject.setVisible(false);
+            detailanchorpane.setVisible(false);
+            SearchProject.setVisible(true);
+            SearchProject.toFront();
+        }
+
+
         System.out.println(theprojectid);
         int idprjass;
         idprjass = Integer.valueOf(theprojectid);
@@ -319,53 +483,62 @@ public  class relationControllers implements Initializable {
 
     public void HandlClick(ActionEvent event ){
 
-    Object source = event.getSource();
-    if (dashboardmenubtn.equals(source)) {
-        pathfiletxt.setText("/dashboard");
-        SearchProject.setVisible(false);
-        addproject.setVisible(false);
-        detailanchorpane.setVisible(false);
-        listeproject.setVisible(false);
-        dashboard.setVisible(true);
-        dashboard.toFront();
-        System.out.println("salam");
-    } else if (projectlistebtn.equals(source)) {
-        pathfiletxt.setText("/dashboard/My project");
-        SearchProject.setVisible(false);
-        addproject.setVisible(false);
-        detailanchorpane.setVisible(false);
-        dashboard.setVisible(false);
-        listeproject.setVisible(true);
-        listeproject.toFront();
-        System.out.println("salam2");
-    } else if (settingsmenubtn.equals(source)) {
-        pathfiletxt.setText("/dashboard/user/Setting");
-        SearchProject.setVisible(false);
-        addproject.setVisible(false);
-        dashboard.setVisible(false);
-        detailanchorpane.setVisible(false);
-        listeproject.setVisible(true);
-        listeproject.toFront();
-    }else if (teammenubtn.equals(source)) {
-        pathfiletxt.setText("/dashboard/project/Jointeam");
+        Object source = event.getSource();
+        if (dashboardmenubtn.equals(source)) {
+            pathfiletxt.setText("/dashboard");
+            getMyprojects();
+            getMytasks();
+            getMyteams();
+            getNewtaskes();
+            getTodotaskes();
+            getDoneTaskes();
+            SearchProject.setVisible(false);
+            addproject.setVisible(false);
+            settings.setVisible(false);
+            detailanchorpane.setVisible(false);
+            listeproject.setVisible(false);
+            dashboard.setVisible(true);
+            dashboard.toFront();
+            System.out.println("salam");
+        } else if (projectlistebtn.equals(source)) {
+            pathfiletxt.setText("/dashboard/My project");
+            SearchProject.setVisible(false);
+            addproject.setVisible(false);
+            detailanchorpane.setVisible(false);
+            settings.setVisible(false);
+            dashboard.setVisible(false);
+            listeproject.setVisible(true);
+            listeproject.toFront();
+            System.out.println("salam2");
+        } else if (settingsmenubtn.equals(source)) {
+            pathfiletxt.setText("/dashboard/user/Setting");
+            SearchProject.setVisible(false);
+            addproject.setVisible(false);
+            dashboard.setVisible(false);
+            detailanchorpane.setVisible(false);
+            settings.setVisible(true);
+            settings.toFront();
+        }else if (teammenubtn.equals(source)) {
+            pathfiletxt.setText("/dashboard/project/Jointeam");
+            settings.setVisible(false);
+            addproject.setVisible(false);
+            dashboard.setVisible(false);
+            listeproject.setVisible(false);
+            detailanchorpane.setVisible(false);
+            SearchProject.setVisible(true);
+            SearchProject.toFront();
+        } else if (addprj.equals(source)) {
+            pathfiletxt.setText("/dashboard/project/AddProject");
+            SearchProject.setVisible(false);
+            dashboard.setVisible(false);
+            listeproject.setVisible(false);
+            detailanchorpane.setVisible(false);
+            settings.setVisible(false);
+            addproject.setVisible(true);
+            addproject.toFront();
+        }
 
-        addproject.setVisible(false);
-        dashboard.setVisible(false);
-        listeproject.setVisible(false);
-        detailanchorpane.setVisible(false);
-        SearchProject.setVisible(true);
-        SearchProject.toFront();
-    } else if (addprj.equals(source)) {
-        pathfiletxt.setText("/dashboard/project/AddProject");
-        SearchProject.setVisible(false);
-        dashboard.setVisible(false);
-        listeproject.setVisible(false);
-        detailanchorpane.setVisible(false);
-        addproject.setVisible(true);
-        addproject.toFront();
     }
-
-}
 
     @FXML
     private void CloseApp(ActionEvent event){
@@ -410,6 +583,138 @@ public  class relationControllers implements Initializable {
 
     }
 
+    /////////////////////////////////////////////////////////
+    //get my projects
+
+    private void getMyprojects(){
+
+        //String querey="select count (*) AS pr from project ";
+        con = db.getConnection();
+        try{
+            preparedStatement = con.prepareStatement("select count(pr.idProjet) from project pr\n" +
+                    "join participed p\n" +
+                    "on p.Team_idTeam=pr.idTeam\n" +
+                    "join user u\n" +
+                    "on u.idUtilisateur=p.userid\n" +
+                    "AND u.idUtilisateur="+UserSession.instance.getID());
+            ResultSet rs=preparedStatement.executeQuery();
+            while (rs.next()){
+                int countpr=rs.getInt(1);
+                l1.setText(String.valueOf(countpr));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    //get my teams
+
+
+    private void getMyteams(){
+
+        //String querey="select count (*) AS pr from project ";
+        con = db.getConnection();
+        try{
+            preparedStatement = con.prepareStatement("select count(u.idUtilisateur) AS MYTEAMS\n" +
+                    " from user u\n" +
+                    "cross join participed p \n" +
+                    "on u.idUtilisateur =p.userid\n" +
+                    "AND u.idUtilisateur="+UserSession.instance.getID());
+            ResultSet rs=preparedStatement.executeQuery();
+            while (rs.next()){
+                int countteams=rs.getInt(1);
+                l3.setText(String.valueOf(countteams));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+    //get my tasks
+
+    private void getMytasks(){
+
+        String querey="select count(*) from tache where participerid = ?" ;
+        con = db.getConnection();
+        try{
+            preparedStatement = con.prepareStatement(querey,Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setInt(1,UserSession.instance.getID());
+            ResultSet rs= preparedStatement.executeQuery();
+            while (rs.next()){
+                int count =rs.getInt(1);
+                nbtask_label1.setText(String.valueOf(count));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getNewtaskes(){
+        String querey="select count(*) from tache where participerid = ? and Status = 'A'" ;
+        con = db.getConnection();
+        try{
+            preparedStatement = con.prepareStatement(querey,Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setInt(1,UserSession.instance.getID());
+            ResultSet rs= preparedStatement.executeQuery();
+            while (rs.next()){
+                int count =rs.getInt(1);
+                nb_new_taskes.setText(String.valueOf(count));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getTodotaskes(){
+        String querey="select count(*) from tache where participerid = ? and Status = 'P'" ;
+        con = db.getConnection();
+        try{
+            preparedStatement = con.prepareStatement(querey,Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setInt(1,UserSession.instance.getID());
+            ResultSet rs= preparedStatement.executeQuery();
+            while (rs.next()){
+                int count =rs.getInt(1);
+                nb_todo_taskes.setText(String.valueOf(count));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getDoneTaskes(){
+        String querey="select count(*) from tache where participerid = ? and Status = 'D'" ;
+        con = db.getConnection();
+        try{
+            preparedStatement = con.prepareStatement(querey,Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setInt(1,UserSession.instance.getID());
+            ResultSet rs= preparedStatement.executeQuery();
+            while (rs.next()){
+                int count =rs.getInt(1);
+                nb_done_taskes.setText(String.valueOf(count));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+////////////////////////////////////////////////////////////////////////////////////
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -417,6 +722,14 @@ public  class relationControllers implements Initializable {
 
         sessionName.setText(UserSession.instance.getUserName());
         sessionId.setText(String.valueOf(UserSession.instance.getID()));
+        Timenow();
+        datenow();
+        getMyprojects();
+        getMytasks();
+        getMyteams();
+        getNewtaskes();
+        getTodotaskes();
+        getDoneTaskes();
         System.out.println("Ara hna");
 
         prjlisten = new ProjectListener() {
